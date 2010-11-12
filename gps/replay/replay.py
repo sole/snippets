af2741f5a47b2aa0d6eab9c7275d96435cfaeee6
sole <listas@soledadpenades.com>
2010-11-12 18:22:57 +0000

    gps/replay new version with support for timestamped placemarks

--- gps/replay/replay.py
+++ gps/replay/replay.py
@@ -1,6 +1,7 @@
 import argparse
 import telnetlib
 import time
+from datetime import datetime
 from xml.dom.minidom import parseString
 
 # Author sole http://soledadpenades.com
@@ -15,6 +16,8 @@ def parse_args():
 
 	parser.add_argument('--interval', type=float, help='Interval between commands, in seconds', default=1)
 
+	parser.add_argument('--realtime', help='Tries to replay using real time intervals (from metadata in each Placemark). If enabled, takes precedence over the interval value', action='store_const', const=True, default=False)
+
 	parser.add_argument('--swap', help='Swap latitude and longitude values.', action='store_const', const=True, default=False)
 	
 	return parser.parse_args()
@@ -25,28 +28,54 @@ def run(args):
 		kml_data = f.read()
 
 	xml = parseString(kml_data)
-
-	coordinate_nodes = xml.getElementsByTagName('coordinates')
-
-	print("Got %d coordinates" % len(coordinate_nodes))
-
 	tn = telnetlib.Telnet('localhost', args.port)
-
 	i = 1
-	for coord in coordinate_nodes:
-		triad = coord.childNodes[0].nodeValue.split(',')
-		if args.swap:
-			tmp_arg = triad[0]
-			triad[0] = triad[1]
-			triad[1] = tmp_arg
-		geo_cmd = ('geo fix ' + (' '.join(triad))).encode('latin-1')
-		print i, geo_cmd
+	d0 = None
+	interval = 0
+	realtime = args.realtime
 
+	placemarks = xml.getElementsByTagName('Placemark')
+	print("Got %d place marks" % len(placemarks))
+		
+	for placemark in placemarks:
+
+		coordinates = placemark.getElementsByTagName('coordinates')
+		if coordinates is None:
+			continue
+					
+		triad = coordinates[0].childNodes[0].nodeValue.split(',')
+		if args.swap:
+			triad[0], triad[1] = triad[1], triad[0]
+		geo_cmd = ('geo fix ' + (' '.join(triad))).encode('latin-1')
+
+		if realtime:	
+			description = placemark.getElementsByTagName('description')
+
+			if description is None:
+				continue
+
+			description = description[0].childNodes[0].nodeValue
+			fix_type, timestamp, accuracy = description.split(':')
+			d = datetime.strptime(timestamp, '%Y%m%d_%H%M%S')
+		
+			if d0 is not None:
+				diff = d - d0
+				interval = (diff.microseconds + (diff.seconds + diff.days * 24 * 3600) * 10**6) / 10**6 
+				print 'difference', diff, interval
+
+			d0 = d
+
+		else:
+			if i > 1:
+				interval = args.interval
+	
+		time.sleep(interval)
+
+		print i, geo_cmd
 		tn.write(geo_cmd + "\r\n")
-		time.sleep(args.interval)
 
 		i = i + 1
-	
+
 	tn.close()
 
 
