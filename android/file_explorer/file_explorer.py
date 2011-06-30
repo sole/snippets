37857b5cb081962f83644b984fe5cfb5c0459a36
sole <listas@soledadpenades.com>
2011-06-30 17:02:34 +0200

    file_explorer: better feedback and arguments info

--- android/file_explorer/file_explorer.py
+++ android/file_explorer/file_explorer.py
@@ -1,4 +1,4 @@
-import os, subprocess, argparse, time
+import os, subprocess, argparse, time, sys
 
 """
 
@@ -13,12 +13,16 @@ def parse_args():
 
 	parser = argparse.ArgumentParser(description='Read/write files from/to Android devices via ADB')
 
-	parser.add_argument('--adb', help="Location of adb binary", default='adb')
-	parser.add_argument('--device_list', help="List device files", default=False)
-	parser.add_argument('--copy_from_device', help='Copy a file (or directory) from the device', default=False)
-	parser.add_argument('--copy_to_host', help='When used with --copy_from_device, specifies where to copy files', default='.')
-	parser.add_argument('--copy_from_host', help='Copy a file (or directory) from the host to the device', default=False)
-	parser.add_argument('--copy_to_device', help='When used with --copy_from_host, specifies where to copy files', default='.')
+	parser.add_argument('--adb', help="Location of adb binary", default='adb', metavar='/path/to/the/adb/executable')
+	parser.add_argument('--device-list', help="List device files", default=False, action='store_const', const=True, dest='device_list')
+	parser.add_argument('--copy-from-device', help='Copy a file (or directory) from the device', default=False, metavar='/path/to/source/in/device', dest='copy_from_device')
+	parser.add_argument('--copy-to-host', help='When used with --copy_from_device, specifies where to copy files', default='.', metavar='/path/to/destination/in/host', dest='copy_to_host')
+	parser.add_argument('--copy-from-host', help='Copy a file (or directory) from the host to the device', default=False, metavar='/path/to/source/in/host', dest='copy_from_host')
+	parser.add_argument('--copy-to-device', help='When used with --copy_from_host, specifies where to copy files', default='.', metavar='/path/to/destination/in/device')
+
+	if len(sys.argv) == 1:
+		parser.print_help()
+		
 
 	return parser.parse_args()
 
